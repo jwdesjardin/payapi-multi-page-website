2226cad213c03d8a20445cf09c95deb26f0c4df1
joey desjardins <jwdesjardin@gmail.com>
2021-05-30 12:20:28 -0700

    add navbar menu with headless ui transition for enter animation

--- my-project/components/Layout/Navbar.tsx
+++ my-project/components/Layout/Navbar.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
+import { Transition } from '@headlessui/react'
 
 const Navbar = () => {
+	const [menu, toggleMenu] = React.useState(false)
+
 	const linkHoverEffect = 'hover:opacity-100 opacity-60 transition-all'
 	return (
 		<header className='flex items-center justify-between content-container py-6 md:py-10'>
-			<div className='flex items-center space-x-12 justify-between xl:space-x-20'>
+			<div className='flex items-center space-x-12 xl:space-x-20'>
 				<LogoSVG></LogoSVG>
-				<div className='hidden md:flex space-x-8 font-bold  '>
+				<div className='hidden md:flex space-x-8 xl:space-x-12 font-bold  '>
 					<Link href='/pricing'>
 						<a className={linkHoverEffect}>Pricing</a>
 					</Link>
@@ -20,12 +23,49 @@ const Navbar = () => {
 				</div>
 			</div>
 
-			<div className='md:hidden'>
+			<div className='md:hidden' onClick={() => toggleMenu(true)}>
 				<HamburgerSVG />
 			</div>
 			<div className='hidden md:block'>
 				<button className='button-primary'>Schedule a Demo</button>
 			</div>
+
+			{/* Menu */}
+
+			<Transition
+				show={menu}
+				enter='transition ease-in-out duration-1000 transform'
+				enterFrom='translate-x-full'
+				enterTo='translate-x-0'
+				leave='transition ease-in-out duration-1000 transform'
+				leaveFrom='translate-x-0'
+				leaveTo='translate-x-full'
+			>
+				<div className='absolute right-0 top-0 h-screen w-10/12 bg-blue-900 md:hidden'>
+					<div className='mt-12 mr-8 flex justify-end' onClick={() => toggleMenu(false)}>
+						<CloseSVG />
+					</div>
+					<div className='border-t border-white-100 border-opacity-60 mx-6 flex-col flex items-center text-[20px] text-white-100 mt-6 '>
+						<Link href='/pricing'>
+							<a className={linkHoverEffect + ' mb-8 mt-10'}>Pricing</a>
+						</Link>
+						<Link href='/about'>
+							<a href='' className={linkHoverEffect + ' mb-8'}>
+								About
+							</a>
+						</Link>
+						<Link href='/contact'>
+							<a href='' className={linkHoverEffect + ' mb-8'}>
+								Contact
+							</a>
+						</Link>
+						{/* Not sure what the functionality of this link should be check back later, no email capture like other similar buttons */}
+						<Link href=''>
+							<button className='button-primary w-full'>Schedule a Demo</button>
+						</Link>
+					</div>
+				</div>
+			</Transition>
 		</header>
 	)
 }
