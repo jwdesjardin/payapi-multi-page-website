e5eacc2ea1622250260035ba7b69fae4a5e6def0
joey desjardins <jwdesjardin@gmail.com>
2021-05-31 12:11:49 -0700

    add hero component to homepage with both inline svg and image solutions

--- my-project/pages/index.tsx
+++ my-project/pages/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import EmailCapture from '../components/EmailCapture'
+import { BgCircleSVG } from '../components/Icons'
+import { HeroImage } from '../components/Icons/illustrations'
 
 import Layout from '../components/Layout'
 
 export default function Home() {
-	return <Layout title='Homepage'></Layout>
+	return (
+		<Layout title='Homepage'>
+			{/* Hero */}
+			<div className='flex flex-col items-center justify-center content-container relative pb-20'>
+				<div className='ml-8 w-52 absolute top-0'>
+					{/* <img src='/home/desktop/mockups.svg' alt='mockups' /> */}
+					<HeroImage></HeroImage>
+				</div>
+				<div className='flex flex-col items-center justify-center pt-[300px]'>
+					<h1 className='text-3xl leading-tight text-center font-serif mb-6'>
+						Start building with our APIs for absolutely free.
+					</h1>
+					<div className='mb-6'>
+						<EmailCapture></EmailCapture>
+					</div>
+					<p className=''>
+						Have any questions? <span className='font-bold'>Contact Us</span>
+					</p>
+				</div>
+			</div>
+		</Layout>
+	)
 }
