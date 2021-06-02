941763d552443d76c512de3790f4e36859754cae
joey desjardins <jwdesjardin@gmail.com>
2021-06-01 18:33:55 -0700

    add styles for about page - mobile screen

--- my-project/pages/about.tsx
+++ my-project/pages/about.tsx
@@ -4,16 +4,21 @@ import Layout from '../components/Layout'
 const about = () => {
 	return (
 		<Layout>
-			<h1>We empower innovators by delivering access to the financial system</h1>
-			<section>
+			<section className='content-container mb-10 pt-4'>
+				<h1 className='text-3xl font-bold font-serif text-center leading-none'>
+					We empower innovators by delivering access to the financial system
+				</h1>
+			</section>
+
+			<section className='content-container flex flex-col items-center space-y-12 mb-16'>
 				<TextSection />
 				<TextSection />
 			</section>
-			<section>
+			<section className=''>
 				<img src='/about/mobile/image-team-members.jpg' alt='' />
 			</section>
 			<StatSection></StatSection>
-			<section>
+			<section className='content-container flex flex-col items-center space-y-12'>
 				<TextSection></TextSection>
 				<TextSection></TextSection>
 			</section>
@@ -25,9 +30,9 @@ export default about
 
 const TextSection = () => {
 	return (
-		<div>
-			<h3>Our Vision</h3>
-			<p>
+		<div className=''>
+			<h3 className='text-2xl font-bold font-serif text-center leading-none mb-4'>Our Vision</h3>
+			<p className='text text-center '>
 				Our main goal is to build beautiful consumer experiences along with developer-friendly
 				infrastructure. The result is an intelligent tool that gives everyone the ability to create
 				amazing products that solve big problems. We are deeply focused on democratizing financial
@@ -39,19 +44,19 @@ const TextSection = () => {
 
 const StatSection = () => {
 	return (
-		<section>
-			<div>
-				<h4>Team Members</h4>
-				<p>300+</p>
-			</div>
-			<div>
-				<h4>Offices in the US</h4>
-				<p>3</p>
-			</div>
-			<div>
-				<h4>Transactions analyzed</h4>
-				<p>10M+</p>
-			</div>
+		<section className='content-container border-b border-t border-grey-400 border-opacity-25 text-center my-12 py-6 flex flex-col items-center space-y-8 '>
+			<StatCard />
+			<StatCard />
+			<StatCard />
 		</section>
+	)
+}
+
+const StatCard = () => {
+	return (
+		<div className=''>
+			<h4 className=''>Team Members</h4>
+			<p className='text-pink-700 text-5xl font-bold font-serif'>300+</p>
+		</div>
 	)
 }
