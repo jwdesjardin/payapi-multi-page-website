3b44ca2caf95eeffc9e5f5d4a7651b334ba44f16
joey desjardins <jwdesjardin@gmail.com>
2021-06-01 17:17:30 -0700

    add dynamic data to pricing component

--- my-project/pages/pricing.tsx
+++ my-project/pages/pricing.tsx
@@ -9,9 +9,48 @@ const pricing = () => {
 					Pricing
 				</h1>
 				<div className='flex flex-col items-center justify-center md:flex-row md:space-x-4'>
-					<PriceComponent></PriceComponent>
-					<PriceComponent></PriceComponent>
-					<PriceComponent></PriceComponent>
+					<PriceComponent
+						title='Free Plan'
+						desc='Build and test using our core set of products with up to 100 API requests'
+						price='$0.00'
+						values={[
+							{ value: 'Transactions', selected: true },
+							{ value: 'Auth', selected: true },
+							{ value: 'Identity', selected: true },
+							{ value: 'Investments', selected: false },
+							{ value: 'Assets', selected: false },
+							{ value: 'Liabilities', selected: false },
+							{ value: 'Income', selected: false },
+						]}
+					></PriceComponent>
+					<PriceComponent
+						title='Basic Plan'
+						desc='Launch your project with unlimited requests and no contractual minimums'
+						price='$249.00'
+						values={[
+							{ value: 'Transactions', selected: true },
+							{ value: 'Auth', selected: true },
+							{ value: 'Identity', selected: true },
+							{ value: 'Investments', selected: true },
+							{ value: 'Assets', selected: true },
+							{ value: 'Liabilities', selected: false },
+							{ value: 'Income', selected: false },
+						]}
+					></PriceComponent>
+					<PriceComponent
+						title='Premium Plan'
+						desc='Get tailored solutions, volume pricing, and dedicated support for your team'
+						price='$499.00'
+						values={[
+							{ value: 'Transactions', selected: true },
+							{ value: 'Auth', selected: true },
+							{ value: 'Identity', selected: true },
+							{ value: 'Investments', selected: true },
+							{ value: 'Assets', selected: true },
+							{ value: 'Liabilities', selected: true },
+							{ value: 'Income', selected: true },
+						]}
+					></PriceComponent>
 				</div>
 			</section>
 		</Layout>
@@ -20,26 +59,33 @@ const pricing = () => {
 
 export default pricing
 
-const PriceComponent = () => {
+const PriceComponent = ({
+	title,
+	desc,
+	price,
+	values,
+}: {
+	title: string
+	desc: string
+	price: string
+	values: {
+		value: string
+		selected: boolean
+	}[]
+}) => {
 	const selected = 'pinkCheckBefore mb-2'
 	const unselected = 'opacity-60 mb-2'
 
 	return (
 		<div className='max-w-sm flex flex-col items-center xl:items-start mb-12'>
-			<h3 className='text-pink-700 text-2xl xl:text-3xl font-serif font-bold mb-4'>Free Plan</h3>
-			<p className='invisible md:visible text-center xl:text-left text mb-4 xl:mr-6'>
-				Build and test using our core set of products with up to 100 API requests
-			</p>
-			<p className='text-5xl md:text-4xl font-bold font-serif mb-6'>$0.00</p>
+			<h3 className='text-pink-700 text-2xl xl:text-3xl font-serif font-bold mb-4'>{title}</h3>
+			<p className='invisible md:visible text-center xl:text-left text mb-4 xl:mr-6'>{desc}</p>
+			<p className='text-5xl md:text-4xl font-bold font-serif mb-6'>{price}</p>
 			<div className='flex justify-center xl:justify-start xl:pl-10 border-b border-t border-grey-400 border-opacity-25 py-6 mb-6 w-full'>
 				<ul className='flex flex-col items-start'>
-					<li className={selected}>Transactions</li>
-					<li className={selected}>Auth</li>
-					<li className={selected}>Identity</li>
-					<li className={selected}>Investments</li>
-					<li className={selected}>Assets</li>
-					<li className={unselected}>Liabilities</li>
-					<li className={unselected}>Income</li>
+					{values.map((item) => (
+						<li className={item.selected ? selected : unselected}>{item.value}</li>
+					))}
 				</ul>
 			</div>
 
