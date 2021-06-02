4fd2f53be74f8b685c40a6cb49e1aae14f61af4f
joey desjardins <jwdesjardin@gmail.com>
2021-06-02 16:09:24 -0700

    collect state and add querystring to link from emailCapture Component

--- my-project/components/EmailCapture.tsx
+++ my-project/components/EmailCapture.tsx
@@ -1,18 +1,31 @@
+import Link from 'next/link'
 import React from 'react'
 
 const EmailCapture = () => {
 	const inputStyles =
 		'py-3 px-8 rounded-full md:rounded-r-none mb-4 md:mb-0 shadow-lg md:shadow-none   '
 	const genericInput = 'focus:outline-none bg-white-100 font-bold text-md w-full '
+
+	const [email, setEmail] = React.useState('')
+
 	return (
 		<div className='shadow-none md:shadow-lg rounded-full inline-flex items-center justify-between max-w-md flex-wrap md:flex-nowrap '>
 			<input
 				type='text'
 				className={inputStyles + ' ' + genericInput}
 				placeholder='Enter email address'
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
 			/>
 			<div className='bg-white-100 rounded-full md:rounded-l-none flex-shrink-0 w-full md:w-auto'>
-				<button className='button-primary w-full md:w-auto'>Schedule a Demo</button>
+				<Link
+					href={{
+						pathname: '/contact',
+						query: { email },
+					}}
+				>
+					<button className='button-primary w-full md:w-auto'>Schedule a Demo</button>
+				</Link>
 			</div>
 		</div>
 	)
